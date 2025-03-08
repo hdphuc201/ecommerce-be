43bb2c0911b04634bf94902e8494eb0350bcb12e
unknown <hdphuc201@gmail.com>
2025-03-09 01:33:15 +0700

    fix: bind server to 0.0.0.0 for Render deployment

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,19 +1,10 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
-
-import "dotenv/config";
-
 export const env = {
   MONGODB_URI: process.env.MONGODB_URI,
   DATABASE_NAME: process.env.DATABASE_NAME,
-  APP_HOST: process.env.APP_HOST,
-  APP_PORT: process.env.APP_PORT,
+  APP_HOST: process.env.APP_HOST || "0.0.0.0",
+  APP_PORT: process.env.PORT || 8017, // Sử dụng PORT thay vì APP_PORT
   ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
   REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
-
   BUILD_MODE: process.env.BUILD_MODE,
-  AUTHOR: process.env.AUTHOR
+  AUTHOR: process.env.AUTHOR,
 };
