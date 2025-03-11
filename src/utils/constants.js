f2b8d604d2e55f462cfc5438cac76d16b3f8946a
unknown <hdphuc201@gmail.com>
2025-03-11 10:08:46 +0700

    fix: fix whitedomain delete '/'

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,6 +2,6 @@
 export const WHITELIST_DOMAINS = [
   // "http://localhost:3000",
   // Không cần local nữa vì ở file config/cors đã luôn luôn cho phép môi trường dev (env.BUILD_MODE === 'dev)
-  "https://hdpstore.vercel.app/",
+  "https://hdpstore.vercel.app",
   //...vv, ví dụ sau này sẽ deloy lên domain chính thức...vv
 ];
