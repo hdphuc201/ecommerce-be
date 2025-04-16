444d0bce00bf7885381573f5dd614be2ba401de1
unknown <hdphuc201@gmail.com>
2025-04-16 21:36:54 +0700

    feat: update

--- src/services/userService.js
+++ src/services/userService.js
@@ -46,7 +46,7 @@ const loginUser = async ({ email, password }) => {
       isAdmin: user.isAdmin,
       _id: user._id,
       isLogin: true,
-      ...(env.COOKIE_MODE && { token: { access_token, refresh_token } }),
+      ...(!env.COOKIE_MODE && { token: { access_token, refresh_token } }),
     };
   } catch (error) {
     return { success: false, message: error.message || "Lỗi server" };
